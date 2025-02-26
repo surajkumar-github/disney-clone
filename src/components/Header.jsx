cdbbe13a6ee043aa15dc75fa41493eed1cdc1fd2
Suraj <suraj01515@gmail.com>
2025-02-27 01:16:25 +0530

    update header and styling; change title to 'Disney', add new logos, and enhance layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './../assets/images/logo.png';
+import logo from './../assets/images/logo2.png';
+import userlogo from './../assets/images/user.png';
 import HeaderItem from '../components/HeaderItem';
 
-import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from 'react-icons/hi2';
+import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv,HiPlus } from 'react-icons/hi2';
 
 function Header() {
   const menu = [
     { name: "HOME", icon: HiHome },
     { name: "SEARCH", icon: HiMagnifyingGlass },
-    { name: "WATCHLIST", icon: HiStar },
-    { name: "ORIGINALS", icon: HiPlayCircle },
-    { name: "MOVIES", icon: HiTv },
+    { name: "WATCHLIST", icon: HiPlus },
+    { name: "ORIGINALS", icon: HiStar },
+    { name: "MOVIES", icon: HiPlayCircle },
     { name: "SERIES", icon: HiTv },
   ];
   
   return (
-    <div>
+    <div className='flex items-center gap-8 justify-between p-5'>
+    <div className='flex items-center gap-8'>
+
       <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
       {menu.map((item, index) => (
         <HeaderItem key={index} name={item.name} Icon={item.icon} />
       ))}
+    </div>
+      <img src={userlogo} className='w-[40px] object-cover rounded-full'/>
     </div>
   );
 }
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 function HeaderItem({ name, Icon }) { // Destructuring props
   return (
-    <div>
+    <div className='text-white flex items-center gap-3
+    text-[15px] font-semibold cursor-pointer'>
         <Icon />
         <h2>{name}</h2>
     </div>
