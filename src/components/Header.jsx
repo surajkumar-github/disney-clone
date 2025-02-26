57fab84dadcf2cf5ff51bad8689b92368079ba4b
Suraj <suraj01515@gmail.com>
2025-02-27 03:00:23 +0530

    implement responsive header menu; add toggle functionality for mobile view and enhance header item styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './../assets/images/logo2.png';
 import userlogo from './../assets/images/user.png';
 import HeaderItem from '../components/HeaderItem';
-
+import { HiDotsVertical } from 'react-icons/hi';
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv,HiPlus } from 'react-icons/hi2';
 
 function Header() {
+  const [toggle,setToggle]=useState(false);
   const menu = [
     { name: "HOME", icon: HiHome },
     { name: "SEARCH", icon: HiMagnifyingGlass },
@@ -20,9 +21,24 @@ function Header() {
     <div className='flex items-center gap-8'>
 
       <img src={logo} className="w-[80px] md:w-[115px] object-cover cursor-pointer" />
+      <div className='hidden md:flex gap-8'>
       {menu.map((item, index) => (
         <HeaderItem key={index} name={item.name} Icon={item.icon} />
       ))}
+      </div>
+      <div className='flex md:hidden gap-5'>
+      {menu.map((item, index) => index<3&&(
+        <HeaderItem name={''} Icon={item.icon} />
+      ))}
+      </div>
+      <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+        <HeaderItem name={''} Icon={HiDotsVertical} />
+        {toggle? <div className='absolute mt-3 bg-[#121212] border-[1px] border-white p-3 px-5 py-4'>
+        {menu.map((item, index) => index>2&&(
+        <HeaderItem name={item.name} Icon={item.icon} />
+      ))}
+        </div>:null}
+      </div>
     </div>
       <img src={userlogo} className='w-[40px] object-cover rounded-full cursor-pointer'/>
     </div>
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -4,9 +4,9 @@ function HeaderItem({ name, Icon }) { // Destructuring props
   return (
     <div className='text-white flex items-center gap-3
     text-[15px] font-semibold cursor-pointer hover:underline
-    underline-offset-8'>
+    underline-offset-8 mb-2'>
         <Icon />
-        <h2>{name}</h2>
+        <h2 className=''>{name}</h2>
     </div>
   );
 }
